e686e9a1669fb503ea68a7dc47f8399ff334b740
Brian <bstclair@salesforce.com>
2016-04-21 16:27:51 -0400

    Enabled unit test that was not annotated properly

--- moduleA/src/test/java/moduleA/ClassOneTest.java
+++ moduleA/src/test/java/moduleA/ClassOneTest.java
@@ -4,8 +4,11 @@ import static org.junit.Assert.assertTrue;
 
 import java.io.PrintStream;
 
+import org.junit.Test;
+
 public class ClassOneTest {
 
+  @Test
   public void testOutput() throws Exception {
     ClassOne classOne = new ClassOne();
     PrintStream out = System.out;
